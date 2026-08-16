65ff26576b217591dabec86cec16c48643b054e0
TDX33029 <tdx33029@gmail.com>
2026-08-16 14:41:27 +0800

    全链路测试通过

--- Simulator/260811/260721/build/Desktop_Qt_6_5_3_GCC_64bit_Debug/CMakeFiles/ps_tracker_ui.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for ps_tracker_ui.
-# This may be replaced when dependencies are built.
--- Simulator/FW/Output/proto.d
+++
@@ -1,4 +0,0 @@
-..\..\output\proto.o: ..\..\User\proto\proto.c
-..\..\output\proto.o: ..\..\User\proto\proto.h
-..\..\output\proto.o: D:\Program Files\Keil5\ARM\ARMCC\Bin\..\include\stdint.h
-..\..\output\proto.o: D:\Program Files\Keil5\ARM\ARMCC\Bin\..\include\stddef.h
